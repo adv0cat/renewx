a87ecbc9df2907d30008a7fa6c03a85e28114df9
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-04-22 01:27:01 +0200

    Add Promise to Freeze type

--- src/utils/freeze.ts
+++ src/utils/freeze.ts
@@ -1,5 +1,5 @@
 import type { Fn } from "../interfaces/core";
 
-export type Freeze<T> = T extends Fn ? T : T extends object ? { readonly [P in keyof T]: Freeze<T[P]> } : Readonly<T>;
+export type Freeze<T> = T extends Fn ? T : T extends Promise<any> ? Readonly<T> : T extends object ? { readonly [P in keyof T]: Freeze<T[P]> } : Readonly<T>;
 
 export const freeze = <T>(value: T | Freeze<T>): Freeze<T> => (Object.isFrozen(value) ? value : Object.freeze(value)) as Freeze<T>
