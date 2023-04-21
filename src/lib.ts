73755187ec3636428ff72037b06a4a1f99a85f09
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-04-21 20:06:21 +0200

    Add export freeze

--- src/lib.ts
+++ src/lib.ts
@@ -2,6 +2,8 @@ export { store } from "./core/store"
 export { join } from "./core/join"
 export { job, allJobs } from "./core/job"
 
+export { freeze } from "./utils/freeze"
+
 export type { Store, Listener } from "./interfaces/store"
 export type { Unsubscribe } from "./interfaces/core"
 export type { Freeze } from "./utils/freeze"
