6c6f7df31a0d212f4a8a08c6f4b9a1c55e24d253
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-04-26 21:06:20 +0200

    Add isNewStateChanged fn as states shallow equal

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -2,3 +2,31 @@ import type { AnyStore, Store } from "../interfaces/store";
 
 export const isNotReadOnlyStore = (store: AnyStore): store is Store<any> =>
   !store.isReadOnly;
+
+const OBJECT = "object";
+export const isNewStateChanged = (oldState: any, newState: any) => {
+  if (oldState === newState) {
+    return false;
+  }
+
+  if (typeof oldState !== OBJECT || oldState === null) {
+    return true;
+  }
+
+  for (let key in newState) {
+    const value = newState[key];
+    const oldValue = oldState[key];
+    if (typeof value === OBJECT && value !== null) {
+      for (let innerKey in value) {
+        const innerValue = value[innerKey];
+        if (typeof innerValue === OBJECT && innerValue !== null) {
+          // Nothing to do... Because we don't need more deepening
+        } else if (innerValue !== oldValue[innerKey]) {
+          return true;
+        }
+      }
+    } else if (value !== oldValue) {
+      return true;
+    }
+  }
+};
