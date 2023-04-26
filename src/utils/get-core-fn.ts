f50998a3255988175d83be9119071233061e7d55
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-04-26 21:07:54 +0200

    Fix getCoreFn with unsubscribes

--- src/utils/get-core-fn.ts
+++ src/utils/get-core-fn.ts
@@ -1,6 +1,7 @@
 import type { Listener, ReadOnlyStore, Notify } from "../interfaces/store";
 import type { ActionInfo } from "../interfaces/action";
 import type { Freeze } from "./freeze";
+import type { Unsubscribe } from "../interfaces/core";
 
 export const getCoreFn = <State>(
   get: ReadOnlyStore<State>["get"],
@@ -11,19 +12,22 @@ export const getCoreFn = <State>(
   ReadOnlyStore<State>["watch"],
   Notify<State>
 ] => {
-  const listeners = [] as Listener<State>[];
+  const unsubscribes = new Map<Listener<State>, Unsubscribe | void>();
   return [
     get,
     id,
-    (listener) => {
-      listeners.push(listener);
-      listener(get(), { actionID: "init" });
-      return () =>
-        listeners.splice(listeners.indexOf(listener), 1)[0]?.(get(), {
-          actionID: `${id()}.#off`,
-        });
+    (listener): Unsubscribe => {
+      unsubscribes.set(listener, listener(get(), { actionID: "init" }));
+      return () => {
+        unsubscribes.get(listener)?.();
+        unsubscribes.delete(listener);
+      };
     },
-    (state: Freeze<State>, info: ActionInfo): void =>
-      listeners.forEach((listener) => listener(state, info)?.()),
+    (state: Freeze<State>, info: ActionInfo): void => {
+      for (const [listener, unsubscribe] of unsubscribes) {
+        unsubscribe?.();
+        unsubscribes.set(listener, listener(state, info));
+      }
+    },
   ];
 };
