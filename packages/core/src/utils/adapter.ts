64bd582581337bfeb59d707d64b00d83952a2fba
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-09-17 03:14:17 +0700

    Fix adapter type

--- packages/core/src/utils/adapter.ts
+++ packages/core/src/utils/adapter.ts
@@ -1,27 +1,32 @@
 import type { Freeze } from "./freeze";
 import type { AnyStore, ReadOnlyStore } from "./store";
 import type { AdapterTag } from "./tag";
+import type { Config } from "./config";
 
 export interface Adapter {
   <ToState extends any[], Stores extends AnyStore[]>(
     stores: [...Stores],
     action: (...state: AdapterStoresType<Stores>) => [...ToState],
     name?: string,
+    config?: Partial<Config>,
   ): AdapterStore<ToState>;
   <ToState, Stores extends AnyStore[]>(
     stores: [...Stores],
     action: (...state: AdapterStoresType<Stores>) => ToState,
     name?: string,
+    config?: Partial<Config>,
   ): AdapterStore<ToState>;
   <ToState extends any[], Store extends AnyStore>(
     store: Store,
     action: (state: AdapterStoreType<Store>) => [...ToState],
     name?: string,
+    config?: Partial<Config>,
   ): AdapterStore<ToState>;
   <ToState, Store extends AnyStore>(
     store: Store,
     action: (state: AdapterStoreType<Store>) => ToState,
     name?: string,
+    config?: Partial<Config>,
   ): AdapterStore<ToState>;
 }
 
