f19900cc9310f3b56ce28b86d069e75f96d2c7ca
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-09-22 05:12:27 +0700

    Add config to logger

--- packages/logger/src/initLogger.ts
+++ packages/logger/src/initLogger.ts
@@ -12,30 +12,34 @@ export const initLogger = (log: (...data: any[]) => void): Unsubscribe => {
     const store = StoreAPI.storeById(storeID);
     if (store !== undefined) {
       const storeName = store.name();
-      watch(store, (v, info) => {
-        if (info === undefined) {
-          return;
-        }
-
-        const { id: actionID, path: storeIDList, set } = info;
-        if (actionID === -1) {
-          log(`${storeName}.#init:`, v);
-        } else {
-          const actionName = ActionAPI.nameById(actionID);
-          if (storeIDList.length === 1) {
-            log(`${storeName}.${actionName}:`, v);
-          } else {
-            const pathOfAction = storeIDList
-              .map(
-                (storeID, index) =>
-                  StoreAPI.storeById(storeID)?.name() +
-                  (index === 0 ? `.${actionName}` : ""),
-              )
-              .join(" --> ");
-            log(`${pathOfAction}.#${set ? "set" : "up"}:`, v);
+      watch(
+        store,
+        (v, info) => {
+          if (info === undefined) {
+            return;
           }
-        }
-      });
+
+          const { id: actionID, path: storeIDList, set } = info;
+          if (actionID === -1) {
+            log(`${storeName}.#init:`, v);
+          } else {
+            const actionName = ActionAPI.nameById(actionID);
+            if (storeIDList.length === 1) {
+              log(`${storeName}.${actionName}:`, v);
+            } else {
+              const pathOfAction = storeIDList
+                .map(
+                  (storeID, index) =>
+                    StoreAPI.storeById(storeID)?.name() +
+                    (index === 0 ? `.${actionName}` : ""),
+                )
+                .join(" --> ");
+              log(`${pathOfAction}.#${set ? "set" : "up"}:`, v);
+            }
+          }
+        },
+        { optimizeStateChange: false },
+      );
     }
   });
 
