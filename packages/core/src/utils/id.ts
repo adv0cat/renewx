b5cca9834e2a6878e8aac115f9145a6706991709
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2023-09-27 04:21:52 +0700

    Refactor unused counter

--- packages/core/src/utils/id.ts
+++ packages/core/src/utils/id.ts
@@ -1,6 +1,5 @@
 export type StoreID = number;
 export type ActionID = number;
-export type JobID = number;
 
 const idCounter = <ID extends number>(): (() => ID) => {
   let id = 0;
@@ -9,4 +8,3 @@ const idCounter = <ID extends number>(): (() => ID) => {
 
 export const nextStoreId = idCounter<StoreID>();
 export const nextActionId = idCounter<ActionID>();
-export const nextJobId = idCounter<JobID>();
