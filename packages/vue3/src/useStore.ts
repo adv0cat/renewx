441f7d9812ccba215884b4c92ab9726fc99a7155
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2024-01-30 23:09:19 +0530

    Fix vue3 bug with duplicate watchers

--- packages/vue3/src/useStore.ts
+++ packages/vue3/src/useStore.ts
@@ -4,19 +4,42 @@ import {
   onScopeDispose,
   type ShallowRef,
 } from "vue";
-import { type AnyStore, type Freeze, isActionStore, watch } from "@renewx/core";
+import {
+  type AnyActionStore,
+  type AnyStore,
+  type Freeze,
+  watch,
+} from "@renewx/core";
 
 const CACHE: ShallowRef<Freeze<any>>[] = [];
+const CACHE_TRACK: (() => void)[] = [];
 
-export function useStore<T>(store: AnyStore<T>): ShallowRef<Freeze<T>> {
-  return (CACHE[store.id] ??= customRef(
+const trackStore = (
+  store: AnyStore,
+  withOff: boolean,
+  trigger = CACHE_TRACK[store.id],
+  unWatch = trigger && getCurrentScope() && watch(store, trigger),
+) =>
+  unWatch &&
+  onScopeDispose(
+    () =>
+      (withOff &&
+        delete CACHE[store.id] &&
+        delete CACHE_TRACK[store.id] &&
+        store.off()) ||
+      unWatch(),
+  );
+
+export const useStore = <T>(
+  store: AnyStore<T>,
+  withOff = false,
+): ShallowRef<Freeze<T>> =>
+  trackStore(store, withOff) ||
+  CACHE[store.id] ||
+  (CACHE[store.id] = customRef(
     (track, trigger) =>
-      (getCurrentScope() && onScopeDispose(watch(store, trigger))) || {
-        get: () => {
-          track();
-          return store.get();
-        },
-        set: isActionStore(store) ? store.set : trigger,
+      trackStore(store, withOff, (CACHE_TRACK[store.id] = trigger)) || {
+        get: () => (track() as undefined) || store.get(),
+        set: (store as AnyActionStore).set || trigger,
       },
   ));
-}
