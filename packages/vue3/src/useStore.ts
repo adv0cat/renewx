ea10bd7660961b71c78ad86232d447338853fb54
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2024-02-02 00:09:07 +0530

    Add config to useStore

--- packages/vue3/src/useStore.ts
+++ packages/vue3/src/useStore.ts
@@ -7,23 +7,28 @@ import {
 import {
   type AnyActionStore,
   type AnyStore,
+  type Config,
   type Freeze,
   watch,
 } from "@renewx/core";
+
+interface Vue3Config extends Config {
+  withOff: boolean;
+}
 
 const CACHE: ShallowRef<Freeze<any>>[] = [];
 const CACHE_TRACK: (() => void)[] = [];
 
 const trackStore = (
   store: AnyStore,
-  withOff: boolean,
+  config: Partial<Vue3Config>,
   trigger = CACHE_TRACK[store.id],
-  unWatch = trigger && getCurrentScope() && watch(store, trigger),
+  unWatch = trigger && getCurrentScope() && watch(store, trigger, config),
 ) =>
   unWatch &&
   onScopeDispose(
     () =>
-      (withOff &&
+      (config.withOff &&
         delete CACHE[store.id] &&
         delete CACHE_TRACK[store.id] &&
         store.off()) ||
@@ -32,13 +37,13 @@ const trackStore = (
 
 export const useStore = <T>(
   store: AnyStore<T>,
-  withOff = false,
+  config: Partial<Vue3Config> = {},
 ): ShallowRef<Freeze<T>> =>
-  trackStore(store, withOff) ||
+  trackStore(store, config) ||
   CACHE[store.id] ||
   (CACHE[store.id] = customRef(
     (track, trigger) =>
-      trackStore(store, withOff, (CACHE_TRACK[store.id] = trigger)) || {
+      trackStore(store, config, (CACHE_TRACK[store.id] = trigger)) || {
         get: () => (track() as undefined) || store.get(),
         set: (store as AnyActionStore).set || trigger,
       },
