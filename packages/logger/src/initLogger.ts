e5ee41dfc75cd47a5bfc8d1ccdb0017edc4d7800
adv0cat <crazygeniys+adv0cat.github.com@gmail.com>
2024-05-05 10:44:09 +0500

    feat(logger)!: update initialization process and get isFirst argument from watcher

--- packages/logger/src/initLogger.ts
+++ packages/logger/src/initLogger.ts
@@ -1,51 +1,48 @@
 import { ActionAPI, StoreAPI, type Unsubscribe, watch } from "@renewx/core";
 
-let isInitialized = false;
+let unWatch: 0 | Unsubscribe = 0;
 export const initLogger = (log: (...data: any[]) => void): Unsubscribe => {
-  if (isInitialized) {
-    return () => {};
-  }
-  isInitialized = true;
-
-  ActionAPI.setAddInfo(true);
-  const unsubscribe = StoreAPI.watch((storeID) => {
-    const store = StoreAPI.storeById(storeID);
-    if (store !== undefined) {
-      const storeName = store.name();
-      watch(
-        store,
-        (v, info) => {
-          if (info === undefined) {
-            return;
-          }
-
-          const { id: actionID, path: storeIDList } = info;
-          if (~actionID) {
-            const actionName = ActionAPI.nameById(actionID);
-            if (storeIDList.length === 1) {
-              log(`${storeName}.${actionName}:`, v);
-            } else {
-              const pathOfAction = storeIDList
-                .map(
-                  (storeID, index) =>
-                    StoreAPI.storeById(storeID)?.name() +
-                    (index === 0 ? `.${actionName}` : ""),
-                )
-                .join(" --> ");
-              log(`${pathOfAction}.#up:`, v);
+  if (unWatch === 0) {
+    ActionAPI.setAddInfo(true);
+    const unsubscribe = StoreAPI.watch((storeID) => {
+      const store = StoreAPI.storeById(storeID);
+      if (store !== undefined) {
+        const storeName = store.name();
+        watch(
+          store,
+          (v, isFirst, info) => {
+            if (info !== undefined) {
+              const { id: actionID, path: storeIDList } = info;
+              if (isFirst) {
+                log(`${storeName}.#init:`, v);
+              } else {
+                const actionName = ActionAPI.nameById(actionID);
+                if (storeIDList.length === 1) {
+                  log(`${storeName}.${actionName}:`, v);
+                } else {
+                  const pathOfAction = storeIDList
+                    .map(
+                      (storeID, index) =>
+                        StoreAPI.storeById(storeID)?.name() +
+                        (index === 0 ? `.${actionName}` : ""),
+                    )
+                    .join(" --> ");
+                  log(`${pathOfAction}.#up:`, v);
+                }
+              }
             }
-          } else {
-            log(`${storeName}.#init:`, v);
-          }
-        },
-        { stateCheck: false },
-      );
-    }
-  });
+          },
+          { stateCheck: false },
+        );
+      }
+    });
 
-  return () => {
-    ActionAPI.setAddInfo(false);
-    unsubscribe();
-    isInitialized = false;
-  };
+    unWatch = () => {
+      ActionAPI.setAddInfo(false);
+      unsubscribe();
+      unWatch = 0;
+    };
+  }
+
+  return unWatch;
 };
