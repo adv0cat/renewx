b55738b8bded2a1c0b77f7f3dd31375c22c185aa
Dmytro <53829896+adv0cat@users.noreply.github.com>
2025-06-18 19:39:04 +0700

    Expand tests and add coverage scripts

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import { join } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@renewx/core': join(__dirname, 'packages/core/index.ts'),
+      '@renewx/logger': join(__dirname, 'packages/logger/index.ts'),
+      '@renewx/vue3': join(__dirname, 'packages/vue3/index.ts'),
+    },
+  },
+  test: {
+    coverage: {
+      provider: 'v8'
+    },
+  },
+});
