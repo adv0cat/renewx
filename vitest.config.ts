eec3238b8474249c8a84b01a76c11b81c5709413
Dmytro <53829896+adv0cat@users.noreply.github.com>
2025-06-18 21:22:41 +0500

    fix: exclude default for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vitest/config';
-import { join } from 'path';
+import { defineConfig, configDefaults } from "vitest/config";
+import { join } from "path";
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@renewx/core': join(__dirname, 'packages/core/index.ts'),
-      '@renewx/logger': join(__dirname, 'packages/logger/index.ts'),
-      '@renewx/vue3': join(__dirname, 'packages/vue3/index.ts'),
+      "@renewx/core": join(__dirname, "packages/core/index.ts"),
+      "@renewx/logger": join(__dirname, "packages/logger/index.ts"),
+      "@renewx/vue3": join(__dirname, "packages/vue3/index.ts"),
     },
   },
   test: {
     coverage: {
-      provider: 'v8'
+      provider: "v8",
+      exclude: [...configDefaults.exclude],
     },
   },
 });
